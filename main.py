f59128ff9a8196e0161e318142c039fc256ef980
josephsurrey <josephsurrey2018@gmail.com>
2024-05-27 20:08:13 +1200

    Edit Card
    Now calls the update_cards() function after editing the card

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 # Find the scroll area layout
                 card_layout = self.findChild(QtWidgets.QGridLayout, "scrollAreaGridLayout")
                 # Create a new instance of the card widget
-                monster_card = MonsterCard()
+                monster_card = MonsterCard(self)
 
                 # Set the name of the card
                 monster_card.findChild(QtWidgets.QLabel, "monster_name").setText(monster_name)
@@ -230,13 +230,15 @@ class MonsterCard(QtWidgets.QWidget):
     # Variable to hold the selection mode
     selection_mode = False
 
-    def __init__(self):
+    def __init__(self, window):
         super(MonsterCard, self).__init__()
         # Loads the .ui file for the monster card
         loadUi("monstercard.ui", self)
 
         # Variable for the selection function
         self.selected = False
+
+        self.window = window
 
     def mousePressEvent(self, event):
         """
@@ -274,15 +276,17 @@ class MonsterCard(QtWidgets.QWidget):
 
         edit_card_dialog = AddCardDialog(monster_name, strength, speed, stealth, cunning)
         if edit_card_dialog.exec():
-            # Get the details of the new card
+            # Get the modified details of the card
             monster_name = edit_card_dialog.findChild(QtWidgets.QLineEdit, "monster_name_value").text()
             strength = edit_card_dialog.findChild(QtWidgets.QSpinBox, "strength_stat_value").value()
             speed = edit_card_dialog.findChild(QtWidgets.QSpinBox, "speed_stat_value").value()
             stealth = edit_card_dialog.findChild(QtWidgets.QSpinBox, "stealth_stat_value").value()
             cunning = edit_card_dialog.findChild(QtWidgets.QSpinBox, "cunning_stat_value").value()
 
-            # Add the card to the monster_cards dictionary
+            # Edit the card in the monster_cards dictionary
             monster_cards[monster_name] = {"Strength": strength, "Speed": speed, "Stealth": stealth, "Cunning": cunning}
+
+        self.window.update_cards()
 
 
 class AddCardDialog(QtWidgets.QDialog):
